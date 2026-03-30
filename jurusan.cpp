4b3a39a08cb435f17db9cf63c7c46929d7766803
Akmall10 <akmalprasety53@gmail.com>
2026-03-30 19:31:32 +0700

    update fungsi output jurusan

--- jurusan.cpp
+++ jurusan.cpp
@@ -17,20 +17,27 @@ void input() {
     cin >> kodeJurusan;
 }
 
+// prosedur untuk pengecekan kondisi
+void pilihanJurusan() {
+    switch (kodeJurusan) {
+        case 1:
+            namaJurusan = "Teknologi Informasi";
+            statusKuota = "Tersedia (15 Kursi)";
+            break;
 
+        case 2:
+            namaJurusan = "Teknik Elektro";
+            statusKuota = "Kuota Penuh!";
+            break;
 
-// prosedur output
-void output() {
-    cout << "\n=== HASIL SELEKSI ===" << endl;
-    cout << "Jurusan Pilihan : " << namaJurusan << endl;
-    cout << "Status Kuota    : " << statusKuota << endl;
-    cout << "-----------------------------------" << endl;
+        case 3:
+            namaJurusan = "Sistem Informasi";
+            statusKuota = "Tersedia (5 Kursi)";
+            break;
+
+        default:
+            namaJurusan = "Tidak Diketahui";
+            statusKuota = "Error: Kode Tidak Valid";
+    }
 }
 
-int main() {
-    input();
-    pilihanJurusan();
-    output();
-
-    return 0;
-}
